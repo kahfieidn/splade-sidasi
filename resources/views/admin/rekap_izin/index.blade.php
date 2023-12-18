0c58dc5f35fc00dfa505d5e5c8ee0ad54bd55c51
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2023-12-18 12:18:33 +0700

    fix mobile ui

--- resources/views/admin/rekap_izin/index.blade.php
+++ resources/views/admin/rekap_izin/index.blade.php
@@ -7,7 +7,7 @@
         <div class="bg-white p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
             <section class="bg-gray-50 dark:bg-gray-900">
                 <div class="py-8 px-4 mx-auto w-full lg:py-5 ">
-                    <div class="w-full space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
+                    <div class="w-full space-y-8 p-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
                     <h2 class="mb-4 text-2xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl dark:text-white">Rekapitulasi Perizinan Sekabupaten Kota</h2>
                         <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Sistem data DPMPTSP ini diperuntukkan bagi DPMPTSP Sekabupaten Kota untuk pelaporan OSS dan Non OSS.</p>
                         <x-splade-form method="GET" action="{{route('admin-rekap-izin.getRekap')}}" class="mt-4 space-y-6">
