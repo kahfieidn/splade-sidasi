f137aaddaf2ee22d1c3bd796b1e2ed1619cdb3d0
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-05-15 10:57:59 +0700

    updarte

--- resources/views/homepage/statistik_investasi/index.blade.php
+++ resources/views/homepage/statistik_investasi/index.blade.php
@@ -2,10 +2,6 @@
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
 
-        <div>
-            
-        </div>
-
             <div class="grid md:grid-cols-2 gap-8">
                 <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
                     <a href="#" class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
