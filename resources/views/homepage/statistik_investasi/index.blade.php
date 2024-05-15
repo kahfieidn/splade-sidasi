68ced76f2d1bd1ab769911c6013e3c8b2f47b8ee
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-05-15 10:08:54 +0700

    adding index investasi

--- resources/views/homepage/statistik_investasi/index.blade.php
+++ resources/views/homepage/statistik_investasi/index.blade.php
@@ -1,5 +1,31 @@
 <x-homepage-layout>
     <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
 
+            <div class="grid md:grid-cols-2 gap-8">
+                <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                    <a href="#" class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
+                        <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                            <path d="M17 11h-2.722L8 17.278a5.512 5.512 0 0 1-.9.722H17a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1ZM6 0H1a1 1 0 0 0-1 1v13.5a3.5 3.5 0 1 0 7 0V1a1 1 0 0 0-1-1ZM3.5 15.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM16.132 4.9 12.6 1.368a1 1 0 0 0-1.414 0L9 3.55v9.9l7.132-7.132a1 1 0 0 0 0-1.418Z" />
+                        </svg>
+                        Information
+                    </a>
+                    <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Data Realtime</h2>
+                    <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Data yang diupdate secara real-time adalah kumpulan informasi yang diperbarui secara instan dan langsung merefleksikan perubahan terbaru. Dalam konteks ini, real-time mengacu pada pengumpulan dan pembaruan data yang hampir seketika atau dengan penundaan sangat minimal setelah terjadinya perubahan di sumber data.</p>
+                    
+                </div>
+                <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                    <a href="#" class="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2">
+                        <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15" />
+                        </svg>
+                        Information
+                    </a>
+                    <h2 class="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">Pusat Satu Data</h2>
+                    <p class="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">Sebagai perangkat daerah dalam penyediaan layanan perizinan dan non-perizinan di Provinsi Kepulauan Riau, Pusat Satu Data DPMPTSP (Dinas Penanaman Modal dan Pelayanan Terpadu Satu Pintu) Kepri menjadi pusat unggulan dalam mengelola dan menyediakan data terkait investasi dan perizinan di wilayah tersebut.</p>
+                    
+                </div>
+            </div>
+        </div>
     </section>
 </x-homepage-layout>
