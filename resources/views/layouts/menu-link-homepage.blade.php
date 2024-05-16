0767950190fa0f4a1136c5aa8279b757564d6cca
Mohammad Kahfi <mhmmdkahfi2000@gmail.com>
2024-05-16 08:44:27 +0700

    added menu link review

--- resources/views/layouts/menu-link-homepage.blade.php
+++ resources/views/layouts/menu-link-homepage.blade.php
@@ -8,4 +8,7 @@
     <li>
         <a href="{{ route('statistik-investasi.index') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Statistik Data Investasi</a>
     </li>
+    <li>
+        <a target="__blank" href="https://b4zx1h69mdc.typeform.com/to/qBYYwJmz" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Umpan Balik</a>
+    </li>
 </ul>
